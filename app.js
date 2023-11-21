97f13ca933522618d117a41501518d2eccde6799
Sai Pradeepti <saipradeepti14@gmail.com>
2023-11-21 14:30:09 +0000

    Updating app.js with post method

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
+app.use(express.json()); //middleware
 
 // app.get("/", (req, res) => {
 //   res
@@ -19,10 +20,29 @@ const tours = JSON.parse(fs.readFileSync(`tours-simple.json`));
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
+    result: tours.length,
     data: {
       tours: tours,
     },
     app: "Natours",
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
+  tours.push(newTour);
+  fs.writeFile(`tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
